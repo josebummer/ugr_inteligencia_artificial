62c0e5d04e5864917f4a53fb9f1d321b2af0d497
Jose Antonio <josebummer@users.noreply.github.com>
2017-06-28 19:28:34 +0200

    jose

--- P2 - Los mundos de Belkan (deliberativo)/Comportamientos_Jugador/jugador.hpp
+++ P2 - Los mundos de Belkan (deliberativo)/Comportamientos_Jugador/jugador.hpp
@@ -1,3 +1,11 @@
+/*
+Practica 2: Agentes deliberativos.
+
+Realizada por: Jose Antonio Ruiz Millan
+
+Inteligencia Artificial 2016/2017
+*/
+
 #ifndef COMPORTAMIENTOJUGADOR_H
 #define COMPORTAMIENTOJUGADOR_H
 
